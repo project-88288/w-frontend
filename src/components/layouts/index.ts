b3542233a39c3d16eaae63f93be97fa0df56f158
chaiya srisawat <chaiya@MSI>
2023-07-18 12:01:43 +0700

    add components

---
+++ src/components/layouts/index.ts
@@ -0,0 +1,7 @@
+export {default } from "components/layouts/layout"
+export * from "components/layouts/layout"
+export { default as Container } from "components/layouts/container"
+export * from "components/layouts/container"
+
+
+
